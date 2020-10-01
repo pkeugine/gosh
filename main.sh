652f7995e1f6588858736a18cda13b613b67b1c5
Eugine Park <pkeugine@gmail.com>
2020-10-02 01:42:34 +0900

    modify main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,4 @@
 nim c src/commands/src/help.nim && mv src/commands/src/help src/commands/bin
 nim c src/commands/src/calc.nim && mv src/commands/src/calc src/commands/bin
-cd src/commands/pymath/ && python3 setup.py install
-cd app && clang++ linux/main.cpp
-cd ../../../..
 clear
 go run src/gosh.go
